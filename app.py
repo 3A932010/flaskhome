52d61c915c438a2f0f170ed71d943b2004b24692
3A932010 <jack37699646@gmail.com>
2024-04-02 15:20:24 +0800

    Second Commit

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hellow World'
+
+@app.route('/hellow')
+def hello():
+    return '<h1>Hello Flask</h1>'
+
+if __name__ == '__main__':
+    app.run()
+
