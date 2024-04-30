36ac6cc5f24508d28439a5f672d8734f31c7053f
3A932010 <jack37699646@gmail.com>
2024-04-30 14:27:43 +0800

    Sixth Commit

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+
+app = Flask(__name__, template_folder='templates',
+            static_url_path='/static', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/shopping')
+def shopping():
+    return render_template('shopping.html')
+
+@app.route('/ticket')
+def ticket_saling():
+    return render_template('ticket_saling.html')
+
+@app.route('/welfare')
+def welfare_platform():
+    return render_template('welfare_platform.html')
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
-if __name__ == '__main__':
-    app.run()
-
