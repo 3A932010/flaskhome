461cb00673c817c093e6d8b45aeee67de9b5f486
3A932010 <jack37699646@gmail.com>
2024-05-14 15:58:46 +0800

    Twelfth Commit

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request
 from flask import redirect, url_for
+import hashlib
+import psycopg2
+import dbconn
 app = Flask(__name__, template_folder='templates',
             static_url_path='/static', static_folder='static')
+app.secret_key='fd4723e200261a2271ea912571eaaa1d'
 name = ''
+
+#DB Connection
+def get_db_connection():
+    conn=psycopg2.connect(
+        host=dbconn.host,
+        database=dbconn.database,
+        user=dbconn.user,
+        password=dbconn.password)
+    return conn
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
@@ -22,25 +35,32 @@ def ticket_saling():
 def welfare_platform():
     return render_template('welfare.html')
 
-@app.route('/user/<username>',methods=['GET'])
-def user(username):
-    message = request.args.get('message')
-    return render_template('user.html', name=username,message=message)
-
+@app.route('/user')
+def user():
+    if 'username' in seesion:
+        username=session['username']
+        return render_template('user.html', name=username)
+    else:
+        return redirect(url_for('signin'))
 @app.route('/search', methods=['GET','POST'])
 def search():
-    global name
-    username=name
-    keyword = request.values['keyword']
-    if keyword == '紅燈':
-        message = '紅燈停!'
-    elif keyword == '黃燈':
-        message ='加速通過馬路或停下等候綠燈!'
-    elif keyword == '綠燈':
-        message ='綠燈行!'    
+    if 'username' in session:
+        username =session['username']
+    
+    if request.method =='POST':
+        keyword = request.values['keyword']
+        if keyword == '紅燈':
+            message = '紅燈停!'
+        elif keyword == '黃燈':
+            message ='加速通過馬路或停下等候綠燈!'
+        elif keyword == '綠燈':
+            message ='綠燈行!'    
+        else:
+            message='請重新輸入!'
+        return render_template('user.html', name=username, message=message)
     else:
         message='請使用HTTP POST傳送資料'
-    return render_template('user.html', name=username, message=message)
+        return render_template('result.html', message=message)
 
 @app.route('/member/signin')
 def signin():
@@ -50,8 +70,23 @@ def signin():
 def login():
     if request.method == 'POST':
         username=request.form['username']
-        userpassword=request.form['userpassword']
-        message = '登入成功!'
-        return redirect(url_for('user', username=username,message=message))
+        userpass=request.form['userpassword']
+        md=hashlib.md5()
+        md.update(userpass.encode('utf-8'))
+        hashpass=md.hexdigest()
+
+        conn=get_db_connection()
+        cursor=conn.cursor()
+        SQL=f"SELECT username,userpass FROM account WHERE username='{username}';"
+        cursor.execute(SQL)
+        user=cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if(username==user[0] and hashpass == user[1]):
+            session['username']=username
+            return redirect(url_for('user'))        
     else:
+        if 'username' in session:
+            return redirect(url_for('user'))
         render_template("member/signin.html")
---
+++ dbconn.py
@@ -0,0 +1,4 @@
+host='dpg-cossef21hbls73aq9rc0-a.singapore-postgres.render.com'
+database='smarthome_xc3c'
+user='admin'
+password='u8BldNStY4Mkf3ns98X1FSSwAhVz1cuT'
